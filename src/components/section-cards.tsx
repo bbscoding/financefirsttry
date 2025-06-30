1251fcde4558a826d281b4f21d939b52f5fc1fe5
bbscoding <batuhan5534@gmail.com>
2025-06-30 18:16:52 +0300

    Can create-data//Last commit did not have that info

--- src/components/section-cards.tsx
+++ src/components/section-cards.tsx
@@ -9,8 +9,10 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-
-export function SectionCards() {
+type SectionCardsProps = {
+  transactions: any[]
+}
+export function SectionCards({ transactions }: SectionCardsProps) {
   return (
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
       <Card className="@container/card">
